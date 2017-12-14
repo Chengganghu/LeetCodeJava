be7cd437b28ea161bd0ecfbd49e6714f35e5ab3d
chengganghu <chenglaian@gmail.com>
2017-12-14 16:40:10 +0800

    增加顺时针旋转图片算法

--- src/Lanceur.java
+++ src/Lanceur.java
@@ -9,6 +9,9 @@ public class Lanceur {
         int resapproach3 = s.lengthOfLongestSubstringApproach3("abcabcbb");
         ZigZag zz = new ZigZag();
         String convert = zz.convert("PAYPALISHIRING",3);
+        Rotate r = new Rotate();
+        int[][] array = {{1,2,3,4},{5,7,6,8},{9,10,11,12},{13,14,15,16}};
+        r.Solution(array);
         System.out.println(convert);
         System.out.println(resapproach3);
         System.out.println(resapproach2);
---
+++ src/Rotate.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Chenggang on 17/12/14.
+ */
+public class Rotate {
+    public void Solution(int [][] matrix){
+        int length = matrix[0].length;
+
+        for(int i=0;i<=length/2;i++){
+            for(int j =i;j<=length-2-i;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[length-1-j][i];
+                matrix[length-1-j][i]=matrix[length-1-i][length-1-j];
+                matrix[length-1-i][length-j-1]=matrix[j][length-1-i];
+                matrix[j][length-1-i]=temp;
+            }
+        }
+
+    }
+}
