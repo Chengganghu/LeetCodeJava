9c27b30b8509a6187c75bd7d00e13aecc5418a6d
chengganghu <chenglaian@gmail.com>
2017-12-30 13:38:26 +0800

    stirngtointeger problem

---
+++ src/StringToInteger.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Chenggang on 17/12/16.
+ */
+public class StringToInteger {
+    public int Solution(String str){
+        try{
+            int result = Integer.getInteger(str);
+        }catch(Exception e) {
+
+        }
+        return 0;
+    }
+}
